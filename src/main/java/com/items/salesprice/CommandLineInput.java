c233ea02b37f18495f242d5b1292ac00797cdab5
Arjun Singh <arjun.singh981212@gmail.com>
2021-05-19 03:15:49 +0530

    Initial interface, dummy-concrete-class and tests

---
+++ src/main/java/com/items/salesprice/CommandLineInput.java
@@ -0,0 +1,14 @@
+package com.items.salesprice;
+
+public class CommandLineInput implements ParserValidator{
+
+  public String[] parse(String str) {
+    String[] dummy = {"Hi", "Bye"};
+    return dummy;
+  }
+
+  public boolean validate(String[] strArray) {
+    boolean dummy = false;
+    return dummy;
+  }
+}
---
+++ src/main/java/com/items/salesprice/ParserValidator.java
@@ -0,0 +1,6 @@
+package com.items.salesprice;
+
+public interface ParserValidator {
+  String[] parse(String str);
+  boolean validate(String[] strArray);
+}
